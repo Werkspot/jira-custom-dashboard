79936f38901141615237ad7cad2d1a7b974c74a8
Alex Agile <alejandro.barba@werkspot.nl>
2018-03-30 13:24:32 +0200

    Symfony global layout

---
+++ src/Werkspot/JiraDashboard/SharedKernel/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Werkspot\JiraDashboard\SharedKernel\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class IndexController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke()
+    {
+        return $this->render('base.html.twig');
+    }
+}
